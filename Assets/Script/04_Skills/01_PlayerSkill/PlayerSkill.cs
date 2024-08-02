dd8d55e540508b80e41ad48583d4db1f1f77912f
ShinDongHwan <sdhqaa@naver.com>
2024-08-02 17:56:59 +0900

    TODO Comment 추가

--- Assets/Script/04_Skills/01_PlayerSkill/PlayerSkill.cs
+++ Assets/Script/04_Skills/01_PlayerSkill/PlayerSkill.cs
@@ -16,6 +16,7 @@ public abstract class PlayerSkill : Skill
 
     protected override void CreateAttackPrefab()
     {
+        // TODO: sword를 생성한다가 모든 PlayerSkill의 공통 구조가 아니기 때문에, 여기 있으면 안됨 - Tony, 2024.08.02
         GameObject sword = GameObject.Instantiate(AttackPrefab);
         sword.transform.SetParent(Player.Instance.transform, false);
         sword.GetComponent<AttackCollider>().SetAtk(atk);
