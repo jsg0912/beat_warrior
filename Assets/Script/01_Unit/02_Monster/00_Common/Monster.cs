7ca766ef479572dcfb79988e75a4bdd1358f6b7a
jsg0912 <jsg09121@naver.com>
2024-08-07 03:47:15 +0900

    적 체력 표시

--- Assets/Script/01_Unit/02_Monster/00_Common/Monster.cs
+++ Assets/Script/01_Unit/02_Monster/00_Common/Monster.cs
@@ -6,22 +6,40 @@ public class Monster : MonoBehaviour
     protected Animator anim;
     public MonsterName monsterName;
     public MonsterUnit monsterUnit;
+    private GameObject HpPrefab;
+    private GameObject Hp;
+    private Vector3 HpPos;
 
     void Start()
     {
         anim = GetComponent<Animator>();
         monsterUnit = MonsterList.FindMonster(monsterName);
         monsterUnit.pattern.Initialize(gameObject);
+
+        HpPrefab = Resources.Load("Prefab/EnemyHeart") as GameObject;
+
+
+        Vector3 HpPos = gameObject.transform.position + new Vector3(0, -0.5f, 0);
+        Hp = GameObject.Instantiate(HpPrefab, HpPos, Quaternion.identity);
+
+        Hp.GetComponent<EnemyHp>().SetHp(monsterUnit.unitStat.hp);
+
     }
 
     void Update()
     {
-        monsterUnit.pattern.PlayPattern();
+        if(monsterUnit.isAlive == true)
+        {
+            ShowUI();
+            monsterUnit.pattern.PlayPattern();
+        }
+        
     }
 
     public virtual void GetDamaged(int dmg)
     {
         monsterUnit.unitStat.hp -= dmg;
+        Hp.GetComponent<EnemyHp>().SetHp(monsterUnit.unitStat.hp);
 
         if (monsterUnit.unitStat.hp <= 0)
         {
@@ -31,11 +49,19 @@ public class Monster : MonoBehaviour
 
         anim.SetTrigger("hurt");
     }
-
+    
     protected virtual void Die()
     {
         monsterUnit.SetDead();
         anim.SetTrigger("die");
         Destroy(gameObject, 2.0f);
+        Destroy(Hp);
+        
+    }
+
+    protected virtual void ShowUI()
+    {
+        Hp.GetComponent<Transform>().position = gameObject.transform.position + new Vector3(0, -0.5f, 0);
+
     }
 }
