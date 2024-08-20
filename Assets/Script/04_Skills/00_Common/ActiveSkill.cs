8d5e189a346b31315d5a6409e06dd82867d33974
ShinDongHwan <sdhqaa@naver.com>
2024-08-20 11:52:01 +0900

    TrySkill, UseSkill, SkillMethod 위치 수정: Skill -> ActiveSKill

--- Assets/Script/04_Skills/00_Common/ActiveSkill.cs
+++ Assets/Script/04_Skills/00_Common/ActiveSkill.cs
@@ -3,4 +3,17 @@ using UnityEngine;
 public abstract class ActiveSkill : Skill
 {
     protected ActiveSkill(GameObject unit) : base(unit) { }
+
+    protected virtual void TrySkill()
+    {
+        if (coolTime > 0) return;
+
+        UseSkill();
+
+        unit.GetComponent<MonoBehaviour>().StartCoroutine(CountCoolTime());
+    }
+
+    protected abstract void UseSkill();
+
+    protected abstract void SkillMethod();
 }
