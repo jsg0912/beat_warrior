a718f3bbe323fabeeb5ccc2262448bccf768ddf2
dlwjdeo <87020463+dlwjdeo@users.noreply.github.com>
2024-08-23 21:07:04 +0900

    카메라 움직임

---
+++ Assets/Script/96_Scene/00_Common/MoveCamera.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class MoveCamera : MonoBehaviour
+{
+    public Transform Target;
+    public float speed;
+    
+    public Vector2 center;
+    public Vector2 size;
+
+    private float height;
+    private float width;
+
+    private void Start()
+    {
+        height = Camera.main.orthographicSize;
+        width = height*Screen.width/Screen.height;
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireCube(center, size);
+    }
+
+    private void LateUpdate()
+    {
+        cameraMovement();
+        maxCameraMovement();
+        
+    }
+
+    private void cameraMovement()
+    {
+        transform.position = Vector3.Lerp(transform.position, Target.position, Time.deltaTime * speed);
+        transform.position = new Vector3(transform.position.x, transform.position.y, -10f);
+    }
+
+    private void maxCameraMovement()
+    {
+        float lx = size.x * 0.5f - width;
+        float clampX = Mathf.Clamp(transform.position.x, -lx + center.x, lx + center.x);
+
+        float ly = size.y * 0.5f - height;
+        float clampY = Mathf.Clamp(transform.position.y, -lx + center.y, lx + center.y);
+
+        transform.position = new Vector3(clampX, clampY, -10f);
+    }
+
+}
+
