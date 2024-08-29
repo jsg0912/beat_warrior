9ccdb9e84814bcfde6e2df8ff769c9729b3915d3
ShinDongHwan <sdhqaa@naver.com>
2024-08-29 17:15:49 +0900

    GameManager 추가

---
+++ Assets/Script/00_Common/GameManager.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager _instance;
+
+    public static GameManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = FindObjectOfType<GameManager>();
+                if (_instance == null)
+                {
+                    GameObject go = new GameObject("GameManager");
+                    _instance = go.AddComponent<GameManager>();
+                    DontDestroyOnLoad(go);
+                }
+            }
+            return _instance;
+        }
+    }
+
+    public void Awake()
+    {
+
+    }
+}
