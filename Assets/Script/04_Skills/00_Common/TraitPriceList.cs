4a7fb3c2de850802e7b88eb086d4bc6bf0847bfb
ShinDongHwan <sdhqaa@naver.com>
2024-08-29 17:15:41 +0900

    TraitPriceList 추가

---
+++ Assets/Script/04_Skills/00_Common/TraitPriceList.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+
+public static class TraitPriceList
+{
+    public static Dictionary<SkillName, int> Info = new Dictionary<SkillName, int>
+    {
+        // {SkillName.AppendMaxHP, },
+        // {SkillName.SkillReset, },
+        // {SkillName.DoubleJump, },
+        // {SkillName.AddDmg, },
+        // {SkillName.Execution, },
+        // {SkillName.Resistance, },
+        // {SkillName.AppendAttack, },
+        // {SkillName.CounterAttack, },
+        // {SkillName.KillRecoveryHP, },
+    };
+
+    public static void CheckTraitPriceListValidation()
+    {
+        SkillName[] skillNames = (SkillName[])Enum.GetValues(typeof(SkillName));
+        int start = (int)SkillName.AdditionalSkill + 1;
+
+        for (int i = start; i < (int)SkillName.End; i++)
+        {
+            if (Info.ContainsKey(skillNames[i]) == false)
+            {
+
+            }
+        }
+    }
+}
