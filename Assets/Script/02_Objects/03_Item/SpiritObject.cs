1307dcdd7bcb7caa53226ff025f3896f0a63ea0f
ShinDongHwan <sdhqaa@naver.com>
2024-08-29 17:18:51 +0900

    TODO 추가 및 영혼 양 조정

--- Assets/Script/02_Objects/03_Item/SpiritObject.cs
+++ Assets/Script/02_Objects/03_Item/SpiritObject.cs
@@ -6,7 +6,7 @@ public class SpiritObject : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("Player"))
         {
-            Inventory.Instance.IncreaseSpirit(1);
+            Inventory.Instance.ChangeSpiritNumber(100); // TODO: Monster Drop Value
             Destroy(gameObject);
         }
     }
