332de31193923604bdb275dfc0fec1ca1af132eb
ShinDongHwan <sdhqaa@naver.com>
2024-09-02 00:10:09 +0900

    playerHeight 추가

--- Assets/Script/01_Unit/01_Player/PlayerConstant.cs
+++ Assets/Script/01_Unit/01_Player/PlayerConstant.cs
@@ -7,6 +7,7 @@ public class PlayerConstant
     public const float gravityScale = 5.0f;
     public const float moveSpeed = 7.0f;
     public const float jumpHeight = 20.0f;
+    public const float playerHeight = 1.0f;
 
     public const float invincibilityTime = 0.5f;
 
