53b08ef9a94ba37a96a53bba3171cd16c667eec8
mingku87 <mingku87@naver.com>
2024-09-08 22:55:07 +0900

    ui매니저 오류 코드 삭제

--- Assets/Script/96_Scene/01_UI/SkillUI.cs
+++ Assets/Script/96_Scene/01_UI/SkillUI.cs
@@ -15,13 +15,6 @@ public class SkillUI : MonoBehaviour
     {
         Instance = this;
         DontDestroyOnLoad(this.gameObject);
-
-
-        SkillCoolTimeImg.Add(SkillName.Mark, SkillCoolTimeImgList[0]);
-        SkillCoolTimeImg.Add(SkillName.Dash, SkillCoolTimeImgList[1]);
-        SkillCoolTimeImg.Add(SkillName.Skill1, SkillCoolTimeImgList[2]);
-        SkillCoolTimeImg.Add(SkillName.Skill2, SkillCoolTimeImgList[3]);
-        SkillCoolTimeImg.Add(SkillName.Attack, SkillCoolTimeImgList[4]);
     }
 
     private void Update()
@@ -31,11 +24,6 @@ public class SkillUI : MonoBehaviour
 
     private void UpdateCoolTime()
     {
-        foreach (var skill in SkillCoolTimeImg)
-            skill.Value.fillAmount
-                = 1 - Player.Instance.GetSkillCoolTime(skill.Key) / PlayerSkillConstant.SkillCoolTime[skill.Key];
-
-
         AttackCountView.text = Player.Instance.GetCurrentStat(StatKind.AttackCount).ToString();
     }
 }
