28e379d4b4c2ed1d532a9958fc206c230e2864b7
dlwjdeo <sksdlwjdeo@naver.com>
2024-09-22 18:18:48 +0900

    태그 추가, 수정, 관련 스크립트 수정

--- Assets/Script/02_Objects/02_MapObject/BlockObject.cs
+++ Assets/Script/02_Objects/02_MapObject/BlockObject.cs
@@ -6,11 +6,11 @@ public class BlockObject : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D collision)
     {
         // TODO: 오브젝트 이름으로 찾는 것 금지, Tag로 일괄처리하도록 수정 - 신동환, 2024.09.13
-        if (collision.gameObject.name == "Marker(Clone)")
+        if (collision.tag == "Mark")
         {
             Marker.Destroy(collision.gameObject);
         }
-        else if (collision.gameObject.name == "Arrow(Clone)")
+        else if (collision.tag == "Projectile")
         {
             Arrow.Destroy(collision.gameObject);
         }
