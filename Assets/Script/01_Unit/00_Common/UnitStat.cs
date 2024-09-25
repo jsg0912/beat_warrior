021db7eebb0b2820df4184b76367a81dfce5a967
ShinDongHwan <sdhqaa@naver.com>
2024-09-25 17:19:39 +0900

    InitializeAllBuff 버그 수정(이전 수정 사항이 코드 파악이 잘못 됨)

--- Assets/Script/01_Unit/00_Common/UnitStat.cs
+++ Assets/Script/01_Unit/00_Common/UnitStat.cs
@@ -34,8 +34,8 @@ public class UnitStat
     {
         foreach (StatKind statKind in Enum.GetValues(typeof(StatKind)))
         {
-            ResetBuffPlus(statKind);
-            ResetBuffMultiply(statKind);
+            buffPlus.Add(statKind, 0);
+            buffMultiply.Add(statKind, 0);
         }
     }
 
