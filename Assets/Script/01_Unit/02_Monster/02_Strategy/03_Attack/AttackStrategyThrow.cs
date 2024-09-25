c81bb3a84b14acddd1aadf0864eb9fcdb59c59a5
mingku87 <mingku87@naver.com>
2024-09-25 16:31:37 +0900

    공격 패턴 추가

--- Assets/Script/01_Unit/02_Monster/02_Strategy/03_Attack/AttackStrategyThrow.cs
+++ Assets/Script/01_Unit/02_Monster/02_Strategy/03_Attack/AttackStrategyThrow.cs
@@ -4,8 +4,15 @@ public class AttackStrategyThrow : AttackStrategy
 {
     protected GameObject obj;
 
+    public override void Initialize(Monster monster)
+    {
+        base.Initialize(monster);
+
+        obj = Resources.Load(MonsterConstant.AttackPrefab[monster.monsterName]) as GameObject;
+    }
+
     protected override void UseSkill()
     {
-        Instantiate(obj);
+        GameObject.Instantiate(obj);
     }
 }
