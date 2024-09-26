98c1f9fea9503ef963aa1c3e8208490a71b4cec6
ShinDongHwan <sdhqaa@naver.com>
2024-09-26 15:08:06 +0900

    하드 코딩 수정

--- Assets/Script/01_Unit/02_Monster/02_Strategy/03_Attack/AttackStrategyThrow.cs
+++ Assets/Script/01_Unit/02_Monster/02_Strategy/03_Attack/AttackStrategyThrow.cs
@@ -14,7 +14,7 @@ public class AttackStrategyThrow : AttackStrategy
 
     protected override void SkillMethod()
     {
-        Vector3 offset = new Vector3(0, 0.5f, 0);
+        Vector3 offset = new Vector3(0, MonsterConstant.ThrowObjectYOffset, 0);
         GameObject throwObj = GameObject.Instantiate(obj, monster.transform.position + offset, quaternion.identity);
         throwObj.GetComponent<MonsterAttackCollider>().Initiate(monster);
     }
