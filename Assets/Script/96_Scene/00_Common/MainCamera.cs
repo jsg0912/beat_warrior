225c2a86b3f7696bbe13a66cdefd5b8b71148372
ShinDongHwan <sdhqaa@naver.com>
2024-10-10 22:00:00 +0900

    MainCamera TODO 추가

--- Assets/Script/96_Scene/00_Common/MainCamera.cs
+++ Assets/Script/96_Scene/00_Common/MainCamera.cs
@@ -1,5 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
+// TODO: 어떤 Scene을 만들든 Main Camera에 MainCamera Script  자동으로 들어가도록 방식 개선 - 전체회의, 20241010
 using UnityEngine;
 
 public class MainCamera : MonoBehaviour
