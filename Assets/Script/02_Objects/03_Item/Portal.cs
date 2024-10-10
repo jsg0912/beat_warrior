0d45c1514d4e7c36cbdd9fa57039b79e37e490ed
ShinDongHwan <sdhqaa@naver.com>
2024-10-10 20:17:09 +0900

    Keycode 관련 TODO Comment 추가

--- Assets/Script/02_Objects/03_Item/Portal.cs
+++ Assets/Script/02_Objects/03_Item/Portal.cs
@@ -6,7 +6,7 @@ public class Portal : MonoBehaviour
     {
         if (collision.gameObject.tag == "Player")
         {
-            if (Input.GetKeyDown(KeyCode.P))
+            if (Input.GetKeyDown(KeyCode.P)) // TODO: KeyCode가 안겹치도록 관리해야됨, KeySettingButton or KeyManager 등을 거치도록 - 신동환, 20241010
             {
                 SceneController.Instance.ChangeScene((SceneName)(SceneController.Instance.CurrentScene + 1));
             }
