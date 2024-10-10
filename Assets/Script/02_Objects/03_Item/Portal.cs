ce9156602a9056e595d90d45934588fd92db46ad
ShinDongHwan <sdhqaa@naver.com>
2024-10-10 20:16:21 +0900

    KeyCode 중복 수정

--- Assets/Script/02_Objects/03_Item/Portal.cs
+++ Assets/Script/02_Objects/03_Item/Portal.cs
@@ -1,14 +1,12 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Portal : MonoBehaviour
 {
     private void OnTriggerStay2D(Collider2D collision)
     {
-        if(collision.gameObject.tag == "Player")
+        if (collision.gameObject.tag == "Player")
         {
-            if (Input.GetKeyDown(KeyCode.F))
+            if (Input.GetKeyDown(KeyCode.P))
             {
                 SceneController.Instance.ChangeScene((SceneName)(SceneController.Instance.CurrentScene + 1));
             }
