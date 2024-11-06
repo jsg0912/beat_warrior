5ad103e6f18d56c927cefc3fd2d714a464e3cbc3
dlwjdeo <sksdlwjdeo@naver.com>
2024-11-06 15:56:47 +0900

    배경 에셋 추가, parallaxBackground 추가

---
+++ Assets/Script/02_Objects/02_MapObject/01_Background/ParallaxBackground.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallaxBackground : MonoBehaviour
+{
+    public float parallaxEffectMultiplier = 0.5f;
+
+    private Transform cameraTransform;
+    private Vector3 lastCameraPosition;
+
+    private void Start()
+    {
+        cameraTransform = Camera.main.transform;
+        lastCameraPosition = cameraTransform.position;
+    }
+
+    private void LateUpdate()
+    {
+        Vector3 deltaMovement = cameraTransform.position - lastCameraPosition;
+
+        transform.position += deltaMovement * parallaxEffectMultiplier;
+
+        lastCameraPosition = cameraTransform.position;
+    }
+}
