34b3d1d591d068b52e7c0d53672257ac2589d336
ShinDongHwan <sdhqaa@naver.com>
2024-11-28 20:57:54 +0900

    Main Camera 방식 임시 코드 추가

--- Assets/Script/96_Scene/00_Common/MainCamera.cs
+++ Assets/Script/96_Scene/00_Common/MainCamera.cs
@@ -2,6 +2,21 @@ using UnityEngine;
 
 public class MainCamera : MonoBehaviour
 {
+    // public static MainCamera _instance;
+    // public static MainCamera Instance
+    // {
+    //     get
+    //     {
+    //         if (_instance == null)
+    //         {
+    //             _instance = FindFirstObjectByType();
+    //         }
+    //         return _instance;
+    //     }
+
+    // }
+
+
     Camera main;
     void Start()
     {
