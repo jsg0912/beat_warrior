69e54b73cfcca7a1c8b37bf83ca14308788ed7d5
dlwjdeo <87020463+dlwjdeo@users.noreply.github.com>
2024-12-04 17:37:30 +0900

    Tag, Layer 관리 클래스 추가

---
+++ Assets/Script/00_Common/GameDefinitions.cs
@@ -0,0 +1,32 @@
+public static class Tag
+{
+    public const string Untagged = "Untagged";
+    public const string Respawn = "Respawn";
+    public const string Finish = "Finish";
+    public const string EditorOnly = "EditorOnly";
+    public const string MainCamera = "MainCamera";
+    public const string Player = "Player";
+    public const string GameController = "GameController";
+    public const string Tile = "Tile";
+    public const string Monster = "Monster";
+    public const string Mark = "Mark";
+    public const string Trap = "Trap";
+    public const string Projectile = "Projectile";
+    public const string Wall = "Wall";
+    public const string Base = "Base";
+    public const string Background = "Background";
+}
+
+public static class Layer
+{
+    public const string Default = "Default";
+    public const string TransparentFX = "TransparentFx";
+    public const string IgnoreRaycast = "Ignore Raycast";
+    public const string Player = "Player";
+    public const string Water = "Water";
+    public const string UI = "UI";
+    public const string Tile = "Tile";
+    public const string Monster = "Monster";
+    public const string MiniMap = "MiniMap";
+
+}
