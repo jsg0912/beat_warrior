2f54b174edf9f05c749a30a1bc3fdf847d942ee9
ShinDongHwan <sdhqaa@naver.com>
2024-12-04 17:51:22 +0900

    Tag&Layer Constant

---
+++ Assets/Script/00_Common/LayerConstnat.cs
@@ -0,0 +1,13 @@
+public static class LayerConstant
+{
+    public const string Default = "Default";
+    public const string TransparentFX = "TransparentFx";
+    public const string IgnoreRaycast = "Ignore Raycast";
+    public const string Player = "Player";
+    public const string Water = "Water";
+    public const string UI = "UI";
+    public const string Tile = "Tile";
+    public const string Monster = "Monster";
+    public const string MiniMap = "MiniMap";
+
+}
--- Assets/Script/00_Common/TagConstant.cs
+++ Assets/Script/00_Common/TagConstant.cs
@@ -1,4 +1,4 @@
-public static class Tag
+public static class TagConstant
 {
     public const string Untagged = "Untagged";
     public const string Respawn = "Respawn";
@@ -15,18 +15,4 @@ public static class Tag
     public const string Wall = "Wall";
     public const string Base = "Base";
     public const string Background = "Background";
-}
-
-public static class Layer
-{
-    public const string Default = "Default";
-    public const string TransparentFX = "TransparentFx";
-    public const string IgnoreRaycast = "Ignore Raycast";
-    public const string Player = "Player";
-    public const string Water = "Water";
-    public const string UI = "UI";
-    public const string Tile = "Tile";
-    public const string Monster = "Monster";
-    public const string MiniMap = "MiniMap";
-
 }
