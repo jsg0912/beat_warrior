4e5d3cfca12abe46722d08f93f80e5aa6e8e2876
ShinDongHwan <sdhqaa@naver.com>
2025-01-08 16:00:33 +0900

    Attack Count Image 변경 기능 추가가

--- Assets/Script/96_Scene/01_UI/InGameUI/AttackCountUI.cs
+++ Assets/Script/96_Scene/01_UI/InGameUI/AttackCountUI.cs
@@ -1,16 +1,30 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class AttackCountUI : MonoBehaviour
 {
+    public static AttackCountUI Instance;
     [SerializeField] Text AttackCountView;
+    [SerializeField] Image AttackChargeGauge;
+    [SerializeField] List<Sprite> Max2ChargeImages = new List<Sprite>();
+    [SerializeField] List<Sprite> Max3ChargeImages = new List<Sprite>(); // 나중에 Attack을 3번까지로 늘어나면 사용
 
-    private void Update()
+    public void Awake()
     {
-        ViewAttackCount();
+        Instance = this;
+
     }
-    private void ViewAttackCount()
+    public void Start()
+    {
+        UpdateUI();
+    }
+    public void UpdateUI()
     {
         AttackCountView.text = Player.Instance.GetCurrentStat(StatKind.AttackCount).ToString();
+        if (Player.Instance.GetFinalStat(StatKind.AttackCount) == 2)
+        {
+            AttackChargeGauge.sprite = Max2ChargeImages[Player.Instance.GetCurrentStat(StatKind.AttackCount)];
+        }
     }
 }
