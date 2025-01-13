d3a29a8f6d567e38b618d62a8ea462329f84b771
mingku87 <mingku87@naver.com>
2025-01-13 12:41:38 +0900

    몬스터 랜덤하게 움직이도록 패턴 수정

---
+++ Assets/Script/01_Unit/02_Monster/02_Strategy/02_Move/MoveStrategyRandom.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+
+public class MoveStrategyRandom : MoveStrategy
+{
+    protected bool isStop = false;
+    protected float changeDestTime = 0;
+
+    public override void PlayStrategy()
+    {
+        base.PlayStrategy();
+        ChangeDest();
+    }
+
+    protected override void Move()
+    {
+        if (isStop == true)
+        {
+            monster.SetIsWalking(false);
+            return;
+        }
+
+        base.Move();
+    }
+
+    protected void ChangeDest()
+    {
+        if (changeDestTime > 0)
+        {
+            changeDestTime -= Time.deltaTime;
+            return;
+        }
+
+        int dest = Random.Range(0, 3);
+        switch (dest)
+        {
+            case 0:
+                ChangeDirection();
+                isStop = false;
+                break;
+            case 1:
+                isStop = true;
+                break;
+            case 2:
+                isStop = false;
+                break;
+        }
+        Debug.Log(monster.name + " " + dest);
+
+        changeDestTime = 3 + Random.value * 2;
+    }
+}
