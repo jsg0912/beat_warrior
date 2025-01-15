ca7605e01316846bfe07f0ce49e12284c5424280
jsg0912 <jsg09121@naver.com>
2025-01-15 17:02:46 +0900

    AttackStrategyRushKoppulso 코드

--- Assets/Script/01_Unit/02_Monster/02_Strategy/03_Attack/AttackStrategyRushKoppulso.cs
+++ Assets/Script/01_Unit/02_Monster/02_Strategy/03_Attack/AttackStrategyRushKoppulso.cs
@@ -4,5 +4,11 @@ using UnityEngine;
 
 public class AttackStrategyRushKoppulso : AttackStrategyRush
 {
+    protected override void SkillMethod()
+    {
+        base.SkillMethod();
+        dashDuration = 3.0f;
+        rushSpeed = 10.0f;
 
+    }
 }
