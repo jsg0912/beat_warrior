a201d889dba0ab6986be6f04ad36162869baf375
ShinDongHwan <sdhqaa@naver.com>
2025-01-20 02:24:39 +0900

    ThrowAttackStrategy 포물선 공식화 및 3가지 방식 구현현

--- Assets/Script/01_Unit/02_Monster/02_Strategy/03_Attack/AttackStrategyThrow.cs
+++ Assets/Script/01_Unit/02_Monster/02_Strategy/03_Attack/AttackStrategyThrow.cs
@@ -4,6 +4,7 @@ public class AttackStrategyThrow : AttackStrategyCreate
 {
     private float throwSpeed;
     private float maxHeight;
+    private float gravity = Physics2D.gravity.y;
 
     public AttackStrategyThrow(float throwSpeed, float maxHeight)
     {
@@ -18,11 +19,33 @@ public class AttackStrategyThrow : AttackStrategyCreate
         Vector3 offset = new Vector3(0, MonsterConstant.ThrowObjectYOffset, 0);
         obj.transform.position = monster.transform.position + offset;
 
-        float gravity = Physics2D.gravity.y;
-        // 포물선 운동 시간 공식
         float distance = GetPlayerPos().x - GetMonsterPos().x - MonsterConstant.ThrowObjectYOffset * GetRelativePlayerDirectionFloat();
-        float time = distance / throwSpeed;
-        Vector3 velocity = new Vector3(throwSpeed, -gravity * time / 2, 0);
-        monsterAttackCollider.rb.velocity = velocity;
+
+        monsterAttackCollider.rb.velocity = GetVelocityConstantFlyTime(distance);
     }
+
+    private Vector3 GetVelocityConstantFlyTime(float distance)
+    {
+        float time = Mathf.Sqrt(2 * maxHeight / -gravity);
+        return new Vector3(distance / time, -gravity * time / 2, 0);
+    }
+
+    private Vector3 GetVelocityConstantXSpeed(float distance)
+    {
+        float time = distance / throwSpeed;
+        return new Vector3(throwSpeed, -gravity * time / 2, 0);
+    }
+
+    private Vector3 GetVelocityConstantAmplitude(float distance)
+    {
+        // 포물선 운동 시간 공식
+        float value = -gravity * distance / (throwSpeed * throwSpeed);
+        float theta = 0.5f * Mathf.Asin(value);
+
+        float sinTheta = Mathf.Sin(theta);
+        float cosTheta = Mathf.Cos(theta);
+
+        return new Vector3(throwSpeed * cosTheta * GetRelativePlayerDirectionFloat(), throwSpeed * sinTheta, 0);
+    }
+
 }
