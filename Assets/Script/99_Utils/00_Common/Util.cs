3382f993bd8119db9fe433d9a1f83627e92b2c4d
ShinDongHwan <sdhqaa@naver.com>
2025-01-25 19:33:12 +0900

    MiniMap 기능 구현

--- Assets/Script/99_Utils/00_Common/Util.cs
+++ Assets/Script/99_Utils/00_Common/Util.cs
@@ -129,4 +129,19 @@ public static class Util
 
         return null; // 컴포넌트를 찾지 못한 경우
     }
+
+    public static Vector2 GetLocalSize(SpriteRenderer spriteRenderer)
+    {
+        if (spriteRenderer != null && spriteRenderer.sprite != null)
+        {
+            Vector2 pixelSize = spriteRenderer.sprite.rect.size; // 픽셀 크기
+            float pixelsPerUnit = spriteRenderer.sprite.pixelsPerUnit; // PPU 값
+
+            DebugConsole.Log($"size is {(pixelSize / pixelsPerUnit).y}");
+            return pixelSize / pixelsPerUnit; // 로컬 크기 반환
+        }
+
+        Debug.LogError("SpriteRenderer or Sprite is null!");
+        return Vector2.zero;
+    }
 }
