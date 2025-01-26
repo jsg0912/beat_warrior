224dfeca686ef788dd439c75236b850f29895064
ShinDongHwan <sdhqaa@naver.com>
2025-01-26 13:16:24 +0900

    Skill 쿨타임 임시로 0.1초로 수정

--- Assets/Script/04_Skills/01_PlayerSkill/02_Active/ActiveSkillPlayer.cs
+++ Assets/Script/04_Skills/01_PlayerSkill/02_Active/ActiveSkillPlayer.cs
@@ -32,19 +32,22 @@ public abstract class ActiveSkillPlayer : ActiveSkill
     // TODO: if there are different type of active skill without "attack", then we have to divide this function's contents - SDH, 20250106
     protected override void CreateEffectPrefab()
     {
-        GameObject attackPrefab = GameObject.Instantiate(EffectPrefab);
-
-        attackPrefab.transform.SetParent(Player.Instance.transform, false);
-        Vector3 Scale = attackPrefab.transform.localScale;
-        attackPrefab.transform.localScale = new Vector3(Scale.x * Player.Instance.GetMovingDirectionFloat(), Scale.y, Scale.z);
-
-        attackCollider = attackPrefab.GetComponentInChildren<AttackCollider>();
-
+        bool madeNew = false;
         if (attackCollider == null)
         {
-            DebugConsole.Log("no attack collider " + this.GetType().Name);
-            return;
+            GameObject attackPrefab = GameObject.Instantiate(EffectPrefab);
+
+            attackPrefab.transform.SetParent(Player.Instance.transform, false);
+            Vector3 Scale = attackPrefab.transform.localScale;
+            attackPrefab.transform.localScale = new Vector3(Scale.x * Player.Instance.GetMovingDirectionFloat(), Scale.y, Scale.z);
+
+            attackCollider = attackPrefab.GetComponentInChildren<AttackCollider>();
         }
+        else
+        {
+            Util.SetActive(attackCollider.gameObject, true);
+        }
+
         attackCollider.SetAtk(damageMultiplier);
     }
 
