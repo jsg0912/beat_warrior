327e1f2e6da1c05ad195883909071d54be2f3710
ShinDongHwan <sdhqaa@naver.com>
2025-01-26 13:31:06 +0900

    게임 속도 조절 기능 추가

--- Assets/Script/00_Common/InGameManager.cs
+++ Assets/Script/00_Common/InGameManager.cs
@@ -26,6 +26,7 @@ public class InGameManager : MonoBehaviour
     public void Start()
     {
         SoulPrefab = Resources.Load(PrefabRouter.SoulPrefab) as GameObject;
+        PauseController.instance.SetGameSpeed(0.3f);
     }
 
     public void CreateSoul(Vector3 position)
