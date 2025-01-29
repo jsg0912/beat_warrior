1d5e04655d9d741fc4d8b68d4e8d513859c36b1c
ShinDongHwan <sdhqaa@naver.com>
2025-01-29 14:56:26 +0900

    잇몸이 방향 전환하도록 수정

--- Assets/Script/01_Unit/02_Monster/01_Patterns/ItmomiPattern.cs
+++ Assets/Script/01_Unit/02_Monster/01_Patterns/ItmomiPattern.cs
@@ -2,6 +2,7 @@ public class ItmomiPattern : Pattern
 {
     public ItmomiPattern()
     {
+        MoveBasic = new MoveStrategyRandom();
         Recognize = new RecognizeStrategyRanged();
         Attack = new AttackStrategyThrowItmomi();
     }
