f7b5c58fb785cde4a7c230c2dbf47bed64dc08c6
ShinDongHwan <sdhqaa@naver.com>
2025-02-02 15:26:02 +0900

    SetStatus에 이미 죽은 경우 안 바뀌도록 수정

--- Assets/Script/01_Unit/02_Monster/00_Common/Monster.cs
+++ Assets/Script/01_Unit/02_Monster/00_Common/Monster.cs
@@ -135,6 +135,7 @@ public class Monster : DirectionalGameObject
     public void SetWalkingAnimation(bool isWalk) { _animator.SetBool(MonsterConstant.walkAnimBool, isWalk); }
     public void SetStatus(MonsterStatus status)
     {
+        if (this.status == MonsterStatus.Dead) return; // 만약 이미 Dead인데, 어디선가 다른 Status 변환을 요구하면 무시 - 신동환, 20250202
         if (status == MonsterStatus.Dead)
         {
             SetIsFixedAnimation(false);
