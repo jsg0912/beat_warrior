8506bd3d4fc665e79e053de1fd3836933ddf15ca
ShinDongHwan <sdhqaa@naver.com>
2025-02-02 20:01:16 +0900

    Player Status 주의사항 COmment 추가

--- Assets/Script/01_Unit/01_Player/PlayerStatus.cs
+++ Assets/Script/01_Unit/01_Player/PlayerStatus.cs
@@ -1,3 +1,6 @@
+// Caution, When you add new status or delete status, you must check whether there is a problem in the player animation
+// Caution, If you want to add new status, you must put the new status under the "Dead"
+
 public enum PlayerStatus
 {
     Idle,
