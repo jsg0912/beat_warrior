bee1e00671e8b7d0f35afd95d7101f4a8a8f551d
ShinDongHwan <sdhqaa@naver.com>
2025-02-03 23:09:21 +0900

    점프 높이 수정

--- Assets/Script/01_Unit/02_Monster/02_Strategy/03_Attack/AttackStrategyRolling.cs
+++ Assets/Script/01_Unit/02_Monster/02_Strategy/03_Attack/AttackStrategyRolling.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class AttackStrategyRolling : AttackStrategy
 {
-    public float jumpPower = 3f; // 점프 높이
+    public float jumpPower = 5f; // 점프 높이
     public int numJumps = 1;  // 튀는 횟수
     public float duration = 1f; // 전체 애니메이션 시간
 
@@ -25,17 +25,12 @@ public class AttackStrategyRolling : AttackStrategy
 
     protected override void SkillMethod()
     {
-        float rotateAngle = monster.GetRelativePlayerDirectionFloat() * -1980 * numJumps;
-
         Vector3 target = Player.Instance.transform.position;
         target.y = monster.transform.position.y + jumpPower;
         SetBeforeSkill();
 
         monster.transform.DOJump(target, jumpPower, numJumps, duration)
             .SetEase(Ease.OutQuad); // 부드러운 튀는 느낌
-
-        //monster.spriteRenderer.transform.DORotate(new Vector3(0, 0, rotateAngle), duration, RotateMode.FastBeyond360)
-        //.SetEase(Ease.Linear); // 일정한 속도로 회전
     }
 
     private void SetBeforeSkill()
