41c8a70f0bb28e0f5ab321936272f706317ae8fe
ShinDongHwan <sdhqaa@naver.com>
2025-02-13 19:13:56 +0900

    Mark Slow Gauge 기능 작업

--- Assets/Script/95_UI/04_Cursor/CursorController.cs
+++ Assets/Script/95_UI/04_Cursor/CursorController.cs
@@ -1,18 +1,13 @@
-using System.Collections;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class CursorController : SingletonObject<CursorController>
 {
     [SerializeField] private Animator cursorAnimator;
-    [SerializeField] private GameObject TimeStopBar;
-    [SerializeField] private Image TimeStopBar_Full;
-    Timer timeStopTimer;
+    [SerializeField] private CursorSlowGaugeController cursorSlowGaugeController;
 
     void Start()
     {
         Cursor.visible = false; // 기본 커서 숨기기
-        timeStopTimer = new Timer();
     }
 
     void Update()
@@ -28,31 +23,13 @@ public class CursorController : SingletonObject<CursorController>
     public void SetInGameCursor()
     {
         cursorAnimator.SetTrigger(CursorAnimationTrigger.InGameTrigger);
-        StopGaugeDecrease();
+        cursorSlowGaugeController.TurnOff();
     }
 
     public void SetZoomInCursor()
     {
         cursorAnimator.SetBool(CursorAnimationTrigger.InGameTrigger, false);
         cursorAnimator.SetTrigger(CursorAnimationTrigger.ZoomInTrigger);
-        StartCoroutine(StartGaugeDecrease(PlayerSkillConstant.MarkSlowDuration));
-    }
-
-    private IEnumerator StartGaugeDecrease(float maxTime)
-    {
-        TimeStopBar.SetActive(true);
-        timeStopTimer.Initialize(maxTime);
-        while (timeStopTimer.Tick())
-        {
-            TimeStopBar_Full.fillAmount = timeStopTimer.remainTime / maxTime;
-            yield return null;
-        }
-        TimeStopBar.SetActive(false);
-    }
-
-    private void StopGaugeDecrease()
-    {
-        StopAllCoroutines();
-        TimeStopBar.SetActive(false);
+        cursorSlowGaugeController.TurnOn();
     }
 }
