507ac677f4c3338d5ca472217d3be65f8a8f608e
ShinDongHwan <sdhqaa@naver.com>
2025-02-14 15:53:23 +0900

    MainCamera 중복 생김 방지 1차?

--- Assets/Script/96_Scene/00_Common/MainCamera.cs
+++ Assets/Script/96_Scene/00_Common/MainCamera.cs
@@ -5,8 +5,7 @@ public class MainCamera : SingletonObject<MainCamera>
     public Camera main;
     protected override void Awake()
     {
-        base.Awake();
         main = GetComponent<Camera>();
+        base.Awake();
     }
-
 }
