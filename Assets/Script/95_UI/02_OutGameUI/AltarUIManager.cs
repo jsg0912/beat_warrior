127c6af45d7683d5112e994cef8d1cfc83435539
ShinDongHwan <sdhqaa@naver.com>
2025-02-15 01:53:48 +0900

    UI 간단 수정

--- Assets/Script/95_UI/02_OutGameUI/AltarUIManager.cs
+++ Assets/Script/95_UI/02_OutGameUI/AltarUIManager.cs
@@ -1,11 +1,13 @@
 using System.Linq;
 using TMPro;
+using UnityEngine;
 
 public class AltarUIManager : SingletonObject<AltarUIManager>
 {
     public AltarPopup altarPopup;
     public AltarDetailPopup altarDetailPopup;
     public TMP_Text PlayerSoulView;
+    public ParticleSystem altarParticle;
 
     public SkillName[] salesSkillList;
     int spiritCount => Inventory.Instance.GetSoulNumber();
@@ -13,6 +15,8 @@ public class AltarUIManager : SingletonObject<AltarUIManager>
     void Start()
     {
         salesSkillList = TraitPriceList.Info.Keys.ToArray();
+        var Particle = altarParticle.main;
+        Particle.useUnscaledTime = true;
     }
 
     public void TurnOnAltarUI()
