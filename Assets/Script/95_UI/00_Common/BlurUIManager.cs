fd2dfa940c42949786a47b014c62d15c3eaead01
ShinDongHwan <sdhqaa@naver.com>
2025-02-14 13:32:57 +0900

    TODO Comment 추가: Blur

--- Assets/Script/95_UI/00_Common/BlurUIManager.cs
+++ Assets/Script/95_UI/00_Common/BlurUIManager.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using UnityEngine;
 public class BlurUIManager : SingletonObject<BlurUIManager>
 {
+    // TODO: Blur가 현재 UI에 들어가서 Image로서 UI들 사이에 Blur가 들어가는데, Sprite 사이에서 사용하려면 Blur Code를 만들어서 추가관리 필요 - 신동환, 20250214
     [SerializeField] private List<BlurTypePair> blurTypePairs;
 
     private BlurType currentBlurType = BlurType.None;
