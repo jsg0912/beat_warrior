351dbff367d0067c101508427822e9dbdf27b8ba
dlwjdeo <87020463+dlwjdeo@users.noreply.github.com>
2025-02-15 13:35:59 +0900

    fadeIn효과 적용

--- Assets/Script/95_UI/02_OutGameUI/SettingUIManager.cs
+++ Assets/Script/95_UI/02_OutGameUI/SettingUIManager.cs
@@ -2,6 +2,7 @@ public class SettingUIManager : SingletonObject<SettingUIManager>
 {
     public const SettingContentIndex DefaultSettingContentIndex = SettingContentIndex.SoundSetting;
     public SettingUIPopup settingUIPopup;
+    public FadeInEffect fadeInEffect;
     private SettingContentIndex currentContentIndex;
 
     protected override void Awake()
@@ -20,6 +21,7 @@ public class SettingUIManager : SingletonObject<SettingUIManager>
     public void TurnOffSettingUI()
     {
         settingUIPopup.TurnOffPopup();
+        fadeInEffect.ShowWithFadeIn();
     }
 
     public void ChangeContent(SettingContentIndex newContentIndex)
