ccb03c413e1c9e0a4862a3db5e46bb46fd884e9c
mingku87 <mingku87@naver.com>
2025-02-16 20:35:24 +0900

    보스 애니메이션 컨트롤러 추가

---
+++ Assets/Script/01_Unit/02_Monster/00_Common/BossGergusAnimatorController.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class BossGergusAnimatorController : StateMachineBehaviour
+{
+    private BossGergus bossGergus;
+
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        if (bossGergus == null) bossGergus = animator.GetComponent<BossGergus>();
+
+        if (stateInfo.IsName(MonsterAnimation.Charge)) bossGergus.SetStatus(MonsterStatus.Attack);
+        else if (stateInfo.IsName(MonsterAnimation.AttackEnd))
+        {
+            bossGergus.AttackEnd();
+            //bossGergus.
+        }
+    }
+
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+    }
+
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+    }
+}
