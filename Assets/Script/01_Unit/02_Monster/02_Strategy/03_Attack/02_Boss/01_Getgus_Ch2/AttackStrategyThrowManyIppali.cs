c6ecea9fa7c2be114e83745f102a2d37de0f7f51
mingku87 <mingku87@naver.com>
2025-02-16 20:15:28 +0900

    이빨이 알 뱉는 위치 찾는 함수 구현

--- Assets/Script/01_Unit/02_Monster/02_Strategy/03_Attack/02_Boss/01_Getgus_Ch2/AttackStrategyThrowManyIppali.cs
+++ Assets/Script/01_Unit/02_Monster/02_Strategy/03_Attack/02_Boss/01_Getgus_Ch2/AttackStrategyThrowManyIppali.cs
@@ -1,3 +1,5 @@
+using UnityEngine;
+
 public class AttackStrategyThrowManyIppali : AttackStrategyThrowMany
 {
     public AttackStrategyThrowManyIppali(float throwSpeed, float maxHeight, int throwCountMax, float throwInterval, PoolTag poolTag) : base(throwSpeed, maxHeight, throwCountMax, throwInterval, poolTag)
@@ -8,19 +10,12 @@ public class AttackStrategyThrowManyIppali : AttackStrategyThrowMany
     }
     protected override void SetTargetPosition()
     {
-        /*
-            TODO KMJ: 
-            알은 두 방향으로 뱉는다. 
+        Vector3 startPos = monster.GetMiddlePos();
 
-            위로 뱉을 때는 위로 포물선 1개 아래 직선으로 1개를 소환한다. 
+        if (throwCountMax == 1) startPos.x += (Random.value > 0.5f) ? 2f : -2f;
 
-            포물선은 왼/오른쪽 2층 타일 중 1곳 중앙으로 발사한다. 
+        RaycastHit2D hit = Physics2D.Raycast(startPos, Vector2.down, Mathf.Infinity, LayerMask.GetMask(LayerConstant.Tile));
 
-            2층 타일의 방향은 랜덤으로 결정한다. 
-
-            아래 직선은 보스 입 바로 직선 아래로 발사하여 0층에 소환한다. 
-        */
-
-        targetPosition = GetPlayerPos();
+        targetPosition = hit.point;
     }
 }
