f8bd242d460a5355e4bd8c3d3a94812a97bcb8f0
ShinDongHwan <sdhqaa@naver.com>
2025-02-17 01:01:25 +0900

    파일 수정 및 빌드 오류 수정정

--- Assets/Script/95_UI/01_InGameUI/06_SystemMassage/SignalReceiverManager.cs
+++ Assets/Script/95_UI/01_InGameUI/06_SystemMassage/SignalReceiverManager.cs
@@ -1,30 +1,27 @@
 using UnityEngine;
 using UnityEngine.Playables;
 using UnityEngine.Timeline;
-using UnityEngine.Events;
 using UnityEngine.SceneManagement;
 
 public class SignalReceiverManager : MonoBehaviour
 {
-    public GameObject signalReceiverObject; 
+    public GameObject signalReceiverObject;
     private PlayableDirector timeline;
-
-    
 
     void Start()
     {
-        SceneManager.sceneLoaded += OnSceneLoaded; 
-        FindAndBindTimeline(); 
+        SceneManager.sceneLoaded += OnSceneLoaded;
+        FindAndBindTimeline();
     }
 
     void OnDestroy()
     {
-        SceneManager.sceneLoaded -= OnSceneLoaded; 
+        SceneManager.sceneLoaded -= OnSceneLoaded;
     }
 
     void OnSceneLoaded(Scene scene, LoadSceneMode mode)
     {
-        FindAndBindTimeline(); 
+        FindAndBindTimeline();
     }
 
     void FindAndBindTimeline()
@@ -35,26 +32,22 @@ public class SignalReceiverManager : MonoBehaviour
             var receiver = signalReceiverObject.GetComponent<SignalReceiver>();
             if (receiver != null)
             {
-                
+
                 TimelineAsset timelineAsset = timeline.playableAsset as TimelineAsset;
                 if (timelineAsset != null)
                 {
                     foreach (var track in timelineAsset.GetOutputTracks())
                     {
-                        if (track is SignalTrack) 
+                        if (track is SignalTrack)
                         {
                             timeline.SetGenericBinding(track, receiver);
                             Debug.Log($"씬 변경 후 Signal Receiver 바인딩 성공: {receiver.gameObject.name}");
                             SystemMessageUIManager.Instance.isTimeLinePlaying = true;
-                            return; 
+                            return;
                         }
                     }
                 }
             }
-        }
-        else
-        {
-            Debug.LogWarning("타임라인 또는 Signal Receiver를 찾을 수 없음!");
         }
     }
 }
