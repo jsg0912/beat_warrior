f9defc3c8d51f5ad8640cc995ca5406b84c5ea26
ShinDongHwan <sdhqaa@naver.com>
2025-02-18 22:06:37 +0900

    이빨이 던지기 수정

--- Assets/Script/01_Unit/02_Monster/02_Strategy/03_Attack/02_Boss/01_Getgus_Ch2/AttackStrategyThrowManyIppali.cs
+++ Assets/Script/01_Unit/02_Monster/02_Strategy/03_Attack/02_Boss/01_Getgus_Ch2/AttackStrategyThrowManyIppali.cs
@@ -13,7 +13,7 @@ public class AttackStrategyThrowManyIppali : AttackStrategyThrowMany
     {
         Vector3 startPos = monster.attackCollider.transform.position;
         // TODO: 임시수정
-        startPos.x += RandomSystem.RandomFloat(-targetPosOffset, targetPosOffset);
+        if (throwCountCurrent == 1) startPos.x += RandomSystem.RandomBool(50) ? 5f : -5f;
         RaycastHit2D hit = Physics2D.Raycast(startPos, Vector2.down, Mathf.Infinity, LayerMask.GetMask(LayerConstant.Tile));
 
         targetPosition = hit.point;
