4e0a211be01950ebe0c60212b879c61a15f411ca
jsg0912 <jsg09121@naver.com>
2025-02-26 05:50:07 +0900

    입꾹이가 보스 뱉기 수정

---
+++ Assets/Script/02_Objects/01_Interactable/02_Monster/BossThrow.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using System.Collections;
+
+public class BossThrow : MonsterAttackCollider
+{
+    void Start()
+    {
+        StartCoroutine(DestroyCreate(10.0f));
+    }
+
+    void Update()
+    {
+        if (CheckBase(Vector3.down) || CheckBase(Vector3.left) || CheckBase(Vector3.right)) StartCoroutine(DestroyCreate(0.03f));
+    }
+
+    private bool CheckBase(Vector3 direction)
+    {
+        Collider2D collider = CheckRay(direction).collider;
+        if (collider != null && collider.CompareTag(TagConstant.Base)) return true;
+        return false;
+    }
+    private RaycastHit2D CheckRay(Vector3 direction) { return Physics2D.Raycast(transform.position, direction, 0.3f, LayerMask.GetMask(LayerConstant.Tile)); }
+
+    public override void OnTriggerEnter2D(Collider2D other)
+    {
+        GameObject obj = other.gameObject;
+        if (obj.CompareTag(TagConstant.Player))
+        {
+            Player.Instance.GetDamaged(damage, GetRelativeDirectionToPlayer());
+            MyPooler.ObjectPooler.Instance.ReturnToPool(PoolTag.GurgesThrow, this.gameObject);
+        }
+    }
+
+    public IEnumerator DestroyCreate(float delay = 0.0f)
+    {
+        yield return new WaitForSeconds(delay);
+        MyPooler.ObjectPooler.Instance.ReturnToPool(PoolTag.GurgesThrow, this.gameObject);
+    }
+}
