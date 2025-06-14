cf841109830bc9bc63c5e83afa5947046ff8d6cf
ShinDongHwan <sdhqaa@naver.com>
2025-06-14 15:23:43 +0900

    DamageCalculator 버그 수정

--- Assets/Script/01_Unit/04_Calculator/DamageCalculatorFix.cs
+++ Assets/Script/01_Unit/04_Calculator/DamageCalculatorFix.cs
@@ -5,7 +5,7 @@ public class DamageCalculatorFix : DamageCalculator
     public override int CalculateDamage(int defensePower, int baseAtk, int damageMultiplier = 1)
     {
         // Ensure that the damage is never negative
-        int damage = baseAtk + damageMultiplier - defensePower;
+        int damage = baseAtk * damageMultiplier - defensePower;
         return Mathf.Max(damage, 0);
     }
 }
--- Assets/Script/01_Unit/04_Calculator/DamageCalculatorRandom.cs
+++ Assets/Script/01_Unit/04_Calculator/DamageCalculatorRandom.cs
@@ -5,7 +5,7 @@ public class DamageCalculatorRandom : DamageCalculator
     public override int CalculateDamage(int defensePower, int baseAtk, int damageMultiplier = 1)
     {
         // Ensure that the damage is never negative
-        int damage = baseAtk + damageMultiplier - defensePower;
+        int damage = baseAtk * damageMultiplier - defensePower;
         if (damage < 0) return 0;
 
         return Mathf.Max(damage + Random.Range(-1, 2), 1);
