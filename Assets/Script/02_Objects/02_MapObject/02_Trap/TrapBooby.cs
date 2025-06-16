33a4d4d2c0b376006a907e6f18bbc44a5894b0cf
ShinDongHwan <sdhqaa@naver.com>
2025-06-17 01:44:42 +0900

    Trap 작업

---
+++ Assets/Script/02_Objects/02_MapObject/02_Trap/TrapBooby.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using UnityEngine;
+
+public class TrapBooby : Trap
+{
+    [SerializeField] private Animator animator;
+    protected override void TrapAction()
+    {
+        animator.SetBool("Trap", true);
+        isTriggered = true;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        GameObject obj = collision.gameObject;
+
+        if (obj.CompareTag(TagConstant.Player) && isTriggered == false)
+        {
+            TrapAction();
+            StartCoroutine(SetTrapCoolTime());
+        }
+    }
+
+    protected IEnumerator SetTrapCoolTime()
+    {
+        yield return new WaitForSeconds(duration);
+        animator.SetBool("Trap", false);
+
+        yield return new WaitForSeconds(coolTime);
+        Initialize();
+    }
+}
