38268be39f9f804795c9d101f68b2a23098b61c2
ShinDongHwan <sdhqaa@naver.com>
2025-06-18 12:26:30 +0900

    FadeIn/Out 도중 컷씬 안넘어가도록 수정

--- Assets/Script/95_UI/05_CutScene/CutSceneController.cs
+++ Assets/Script/95_UI/05_CutScene/CutSceneController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class CutSceneController : MonoBehaviour
 {
     public bool isPlaying { get; private set; } = false;
+    private bool isFading = false; // TODO: 버그가 생길 수 있으니 이를 활용한 로직이 많아지면 개선 필요 - SDH, 20250618
     public CutSceneKind cutSceneKind;
     [SerializeField] private TMP_Text cutSceneText;
     [SerializeField] private List<FadeInOutImage> cutSceneObjects;
@@ -16,6 +17,7 @@ public class CutSceneController : MonoBehaviour
 
     public void TutorialCommandCheck()
     {
+        if (isFading) return;
         if (Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.KeypadEnter) || Input.GetMouseButtonDown(0))
         {
             NextCutScene();
@@ -47,6 +49,7 @@ public class CutSceneController : MonoBehaviour
 
         if (currentIndex < cutSceneObjects.Count - 1)
         {
+            isFading = true;
             cutSceneObjects[currentIndex++].HideWithFadeOut(() =>
             {
                 ShowCutScene();
@@ -60,7 +63,7 @@ public class CutSceneController : MonoBehaviour
 
     public void ShowCutScene()
     {
-        cutSceneObjects[currentIndex].ShowWithFadeIn();
+        cutSceneObjects[currentIndex].ShowWithFadeIn(() => { isFading = false; });
         string[] dialogs = DialogScript.CutSceneData[cutSceneKind][GameManager.Instance.Language][currentIndex].Split('\r');
         foreach (string dialog in dialogs)
         {
@@ -73,11 +76,13 @@ public class CutSceneController : MonoBehaviour
     {
         if (cutSceneObjects.Count > 0)
         {
+            isFading = true;
             cutSceneObjects[currentIndex].HideWithFadeOut(() =>
             {
                 currentIndex = 0;
                 cutSceneText.text = string.Empty;
                 isPlaying = false;
+                isFading = false;
                 fadeInEffect.HideWithFadeOut();
                 UIManager.Instance.SetActiveMiniMap(true);
             });
