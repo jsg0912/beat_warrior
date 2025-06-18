1c6a3b3ecc33b4a0d57bc7f31b393a47852aaa4e
ShinDongHwan <sdhqaa@naver.com>
2025-06-19 01:05:49 +0900

    TODO COmment 추가

--- Assets/Script/95_UI/02_OutGameUI/01_Popup/GameOverPopup.cs
+++ Assets/Script/95_UI/02_OutGameUI/01_Popup/GameOverPopup.cs
@@ -17,6 +17,7 @@ public class GameOverPopup : PopupSystem
         GameManager.Instance.QuitInGame();
     }
 
+    // TODO: 이렇게 따로 하지말고, PopupSystem에서 Esc로 무시할 수 있는 애인지 아닌지를 설정할 수 있고, 그것에 따라 Common에서 처리하도록 변경
     // 이렇게 하면, esc로 안사라진다.
     public override bool TurnOnPopup()
     {
